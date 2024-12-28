216abe7afebe9b4cf33fce30c0e7e5e0dbc74020
liv-daliberti <34242908+liv-daliberti@users.noreply.github.com>
2024-12-28 12:13:42 +0000

    Deploying to gh-pages from @ liv-daliberti/liv-daliberti.github.io@1984a68224b26bd93965e9ab1e2e8c1e495787dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-start-my-computer-science-ph-d-at-princeton-university",
+          title: 'Start my Computer Science Ph.D. at Princeton University!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
