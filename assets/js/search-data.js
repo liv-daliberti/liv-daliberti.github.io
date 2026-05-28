a49c6184e2ff53c92af7c0d93eb35226e18fbf0b
liv-daliberti <34242908+liv-daliberti@users.noreply.github.com>
2026-05-28 01:00:55 +0000

    Deploying to gh-pages from @ liv-daliberti/liv-daliberti.github.io@22fa4d9d1716ebd5efec2948742479c44a670286 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,18 +58,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-liv-39-s-thoughts-on-teaching",
-      
-        title: "Liv&#39;s thoughts on teaching",
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2026/livs-thoughts-on-teaching/";
-        
-      },
-    },{id: "post-i-passed-my-generals-exam",
+            },{id: "post-i-passed-my-generals-exam",
       
         title: "I Passed My Generals Exam",
       
